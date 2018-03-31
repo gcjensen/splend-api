4d8ce76c788802a2d8ebfa9fce2d60b5fb3b82a0
George Jensen <george@gcjensen.uk>
2018-03-31 18:01:58 +0100

    Move reusable response methods into their own file

--- endpoints/get_user_details.go
+++ endpoints/get_user_details.go
@@ -2,7 +2,6 @@ package endpoints
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"github.com/gcjensen/settle-api/user"
 	"github.com/julienschmidt/httprouter"
@@ -34,28 +33,4 @@ func GetUserDetails(dbh *sql.DB) httprouter.Handle {
 
 		respondWithJSON(writer, http.StatusOK, user)
 	})
-}
-
-func respondWithError(err error, writer http.ResponseWriter) {
-	var code int
-	var message string
-
-	switch err {
-	case sql.ErrNoRows:
-		code = http.StatusNotFound
-		message = "User not found"
-	default:
-		code = http.StatusInternalServerError
-		message = err.Error()
-	}
-
-	respondWithJSON(writer, code, map[string]string{"error": message})
-}
-
-func respondWithJSON(writer http.ResponseWriter, code int, resp interface{}) {
-	response, _ := json.Marshal(resp)
-
-	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(code)
-	writer.Write(response)
 }
---
+++ endpoints/response.go
@@ -0,0 +1,31 @@
+package endpoints
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+)
+
+func respondWithError(err error, writer http.ResponseWriter) {
+	var code int
+	var message string
+
+	switch err {
+	case sql.ErrNoRows:
+		code = http.StatusNotFound
+		message = "User not found"
+	default:
+		code = http.StatusInternalServerError
+		message = err.Error()
+	}
+
+	respondWithJSON(writer, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(writer http.ResponseWriter, code int, resp interface{}) {
+	response, _ := json.Marshal(resp)
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(code)
+	writer.Write(response)
+}
