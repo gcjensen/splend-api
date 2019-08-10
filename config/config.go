d41be8301cfb17f726cbe3154177d4f22173ba30
George Jensen <george@gcjensen.uk>
2019-08-10 17:10:05 +0100

    Change CI to use mariadb docker image and update config file name

--- config/config.go
+++ config/config.go
@@ -65,7 +65,7 @@ func TestDBH() *sql.DB {
 func Load() *config {
 
 	// Update to point towards your config file
-	configFile, err := ioutil.ReadFile("/etc/splend.yaml")
+	configFile, err := ioutil.ReadFile("/etc/splend-api.yaml")
 
 	if err != nil {
 		log.Printf("configFile.Get err #%v", err)
